b548272717ce0b3e84b7302a1b837ba8d2a1048e
staney41011 <45749474+staney41011@users.noreply.github.com>
2026-01-04 17:29:59 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // 載入 Tailwind 樣式
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
 

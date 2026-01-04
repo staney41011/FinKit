c7d9ce46205843d67f311f6479fb73f098c8c804
staney41011 <45749474+staney41011@users.noreply.github.com>
2026-01-04 18:11:01 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <--- 關鍵！這行一定要有！
+
+// ⚠️ 注意：我移除了 "import './index.css'" 這行
+// 因為我們改用 CDN 了，不需要它，這樣就不會報錯了！
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
